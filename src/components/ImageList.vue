7ac4eff9c2735bf091b851acbff1f05704f2cba2
Andres Betin <andresdavidbetin@gmail.com>
2020-06-26 10:45:26 -0500

    add vuex support

--- src/components/ImageList.vue
+++ src/components/ImageList.vue
@@ -3,8 +3,19 @@
     <div class="grid col-gap-8" :class="gridColsClass">
       <div v-for="(imageList, index) in imageMatrix" :key="index">
         <div v-for="image in imageList" :key="image.id" class="pb-4 text-center">
-          <div class="max-w-sm rounded overflow-hidden shadow-lg  m-auto">
+          <div class="max-w-sm rounded overflow-hidden shadow-lg m-auto">
             <img class="w-full" :src="image.urls.small" :alt="image.alt_description" />
+            <div v-if="!checkSelected(image)" class="w-full px-2 py-4">
+              <button
+                v-on:click="selectImage(image)"
+                class="bg-blue-500 w-full hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              >Agregar</button>
+            </div>
+            <div v-else class="w-full px-2 py-4">
+              <button
+                class="bg-blue-500 w-full text-white font-bold py-2 px-4 rounded opacity-50 cursor-not-allowed"
+              >Agregado!</button>
+            </div>
           </div>
         </div>
       </div>
@@ -15,9 +26,6 @@
 <script>
 export default {
   name: "ImageList",
-  props: {
-    images: Array
-  },
   data: function() {
     return {
       winWidth: 0
@@ -33,11 +41,24 @@ export default {
   methods: {
     handleResize() {
       this.winWidth = window.innerWidth;
+    },
+    selectImage: function(image) {
+      this.$store.dispatch("addImage", image);
+    },
+    checkSelected: function(image) {
+      let listSelectedImages = this.$store.getters.selectedImages;
+      return listSelectedImages.some(img => img.id === image.id);
     }
   },
   computed: {
     columns: function() {
-      return this.winWidth < 768 ? 1 : this.winWidth < 1024 ? 2 : (this.winWidth < 1280 ? 3 : 4);
+      return this.winWidth < 768
+        ? 1
+        : this.winWidth < 1024
+        ? 2
+        : this.winWidth < 1280
+        ? 3
+        : 4;
     },
     gridColsClass: function() {
       return `grid-cols-${this.columns}`;
@@ -46,11 +67,12 @@ export default {
       var matrix = Array(this.columns)
         .fill()
         .map(() => []);
-      for (var i = 0, k = 0; i < this.images.length; i++) {
+      let images = this.$store.getters.loadedImages;
+      for (var i = 0, k = 0; i < images.length; i++) {
         if (i % this.columns === 0) {
           k = 0;
         }
-        matrix[k++].push(this.images[i]);
+        matrix[k++].push(images[i]);
       }
       return matrix;
     }
@@ -58,13 +80,4 @@ export default {
 };
 </script>
 
-<style scoped>
-.masonry-brick {
-  box-sizing: border-box;
-  display: block;
-  height: auto;
-  line-height: 24px;
-  padding-bottom: 40px;
-  width: 100%;
-}
-</style>
+<style scoped></style>
